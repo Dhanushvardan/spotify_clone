bcef94f555cd895c63b6cd11d47945fd0ef1b08e
pranaavs2003 <pranaavs2003@gmail.com>
2023-07-03 00:22:50 +0530

    url change

--- src/spotify.js
+++ src/spotify.js
@@ -1,8 +1,10 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 // Replace with your app's client ID, redirect URI and desired scopes
 const clientId = "dc8a9df2dcb94dcaa855820a2ffdbb77";
-const redirectUri = "https://spotify-clone-lime-nu.vercel.app/#";
-//https://spotify-clone-pranaav.netlify.app
+const redirectUri = "https://spotify-clone-lime-nu.vercel.app/";
+//https://spotify-clone-lime-nu.vercel.app/#
+
+//http://localhost:3000/
 const scopes = [
   "user-read-currently-playing",
   "user-read-recently-played",
