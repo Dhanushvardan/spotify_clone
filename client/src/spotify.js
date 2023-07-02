ca9efe2cc60cac95ef91c33c5a76d073d00f919d
pranaavs2003 <pranaavs2003@gmail.com>
2023-07-02 16:34:17 +0530

    redirect change

--- client/src/spotify.js
+++ client/src/spotify.js
@@ -1,7 +1,7 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 // Replace with your app's client ID, redirect URI and desired scopes
 const clientId = "dc8a9df2dcb94dcaa855820a2ffdbb77";
-const redirectUri = "http://localhost:3000/";
+const redirectUri = "https://spotify-clone-pranaav.netlify.app";
 const scopes = [
   "user-read-currently-playing",
   "user-read-recently-played",
